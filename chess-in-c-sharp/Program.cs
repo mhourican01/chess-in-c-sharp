e35329bcdc16b68eb2c6fe7b1c823a45b7cc15d1
mhourican01 <mhourican01@qub.ac.uk>
2021-02-23 11:42:13 +0000

    Began implementing pieces

--- chess-in-c-sharp/Program.cs
+++ chess-in-c-sharp/Program.cs
@@ -9,6 +9,9 @@ namespace chess_in_c_sharp
 
         static char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
         static ArrayList squares = new ArrayList();
+        static Player p1 = new Player(Colour.White, null);
+        static Player p2 = new Player(Colour.Black, null);
+        static Piece piece;
 
         static void Main(string[] args)
         {
@@ -21,7 +24,34 @@ namespace chess_in_c_sharp
             {
                 for (int nCount = 1; nCount <= letters.Length; nCount++)
                 {
-                    Square square = new Square(letters[lCount], nCount);
+                    if (letters[lCount] == 'a' && (nCount == 1 || nCount == 8))
+                    {
+                        piece = new Piece(Type.Rook);
+                        p1.Pieces.Add(piece);
+                    } else if (letters[lCount] == 'a' && (nCount == 2 || nCount == 7))
+                    {
+                        piece = new Piece(Type.Knight);
+                        p1.Pieces.Add(piece);
+                    } else if (letters[lCount] == 'a' && (nCount == 3 || nCount == 6))
+                    {
+                        piece = new Piece(Type.Bishop);
+                        p1.Pieces.Add(piece);
+                    } else if (letters[lCount] == 'a' && (nCount == 4))
+                    {
+                        piece = new Piece(Type.King);
+                        p1.Pieces.Add(piece);
+                    } else if (letters[lCount] == 'a' && (nCount == 5))
+                    {
+                        piece = new Piece(Type.Queen);
+                        p1.Pieces.Add(piece);
+                    }
+                    if (letters[lCount] == 'b')
+                    {
+                        piece = new Piece(Type.Pawn);
+                        p1.Pieces.Add(piece);
+                    }
+
+                    Square square = new Square(letters[lCount], nCount, piece);
                     squares.Add(square);
                 }
             }
@@ -36,23 +66,6 @@ namespace chess_in_c_sharp
                 if (s.Number == 8)
                 {
                     Console.WriteLine();
-                }
-            }
-        }
-
-        static void CreatePlayers()
-        {
-            Player p1 = new Player(Colour.White, null);
-            Player p2 = new Player(Colour.Black, null);
-        }
-
-        static void CreatePieces()
-        {
-            int pieceCount = 1;
-            while (pieceCount <= maxPieces) { 
-                if (pieceCount <= 8)
-                {
-                    // Piece piece = new Piece(Type.Pawn, squares.);
                 }
             }
         }
